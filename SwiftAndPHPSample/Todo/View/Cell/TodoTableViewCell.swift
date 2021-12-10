7902f328e7c25d4da2c80dc2c048e58d17103c26
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-10 16:10:40 +0900

    todoのセル作成

---
+++ SwiftAndPHPSample/Todo/View/Cell/TodoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoTableViewCell.swift
+//  SwiftAndPHPSample
+//
+//  Created by 坂本龍哉 on 2021/12/10.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
